b682570d2b9eee1441e50110355df852003db02e
CatzillaOrz <catzillaorz@outlook.com>
2022-07-13 16:15:51 +0800

    1 file added

---
+++ 05-LiteralType.ts
@@ -0,0 +1,26 @@
+export class UnionType {
+  constructor() {}
+
+  combine(
+    input1: number | string,
+    input2: number | string,
+    resultConversion: 'as-number' | 'as-text'
+  ) {
+    let result;
+    if (
+      (typeof input1 === 'number' && typeof input2 === 'number') ||
+      resultConversion === 'as-number'
+    ) {
+      result = +input1 + +input2;
+    } else {
+      result = input1.toString() + input2.toString();
+    }
+
+    return result;
+  }
+
+  run() {
+    this.combine(2, 3, 'as-number');
+    this.combine(2, 3, 'as-text');
+  }
+}
