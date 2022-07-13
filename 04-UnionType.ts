21a45d625f208336269bf31afa0ce02db8f69091
CatzillaOrz <catzillaorz@outlook.com>
2022-07-13 16:08:38 +0800

    1 file modified

--- 04-UnionType.ts
+++ 04-UnionType.ts
@@ -6,8 +6,9 @@ export class UnionType {
     if (typeof input1 === 'number' && typeof input2 === 'number') {
       result = input1 + input2;
     } else {
-      result;
+      result = input1.toString() + input2.toString();
     }
+    return result;
   }
 
   run() {
