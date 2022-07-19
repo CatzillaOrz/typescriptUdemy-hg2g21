fe7a0b5044350e4672be4d4ce828eb6fdbb72e2b
CatzillaOrz <catzillaorz@outlook.com>
2022-07-19 12:10:04 +0800

    1 file modified

--- 14-ClassConstructor.ts
+++ 14-ClassConstructor.ts
@@ -1,12 +1,23 @@
 class Department {
   name: string;
 
+  // private employee: string [];
+
   constructor(n: string) {
     this.name = n;
-    this.run();
+    this.describe();
   }
 
-  run() {
+  describe(this: Department) {
     console.log('name:', this.name);
   }
 }
+
+const accounting = new Department('Accounting');
+accounting.describe(); // work normally
+
+const accountingCopy = { describe: accounting.describe };
+// accountingCopy.describe(); // Error
+
+const accountingCopy1 = { name: '', describe: accounting.describe };
+accountingCopy1.describe(); // ok
