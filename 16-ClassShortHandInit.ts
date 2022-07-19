fff241024bc7d1d9640f279a015d7decc48e0214
CatzillaOrz <catzillaorz@outlook.com>
2022-07-19 12:25:32 +0800

    1 file modified

--- 16-ClassShortHandInit.ts
+++ 16-ClassShortHandInit.ts
@@ -2,12 +2,17 @@ class ClassShortHandInit {
   // private id: string;
   // private name: string;
 
-  constructor(private id: string, public name: string) {
+  constructor(
+    private id: string,
+    public name: string,
+    private readonly key: string
+  ) {
     // this.id = id;
     // this.name = name;
   }
 
   describe() {
+    // this.key = 'some xxx'; // Error: readonly Could only be Initialized in new constructor like: new Deparmetn('idxxx', 'name', 'key xxxx')
     console.log('Deparment:' + `${this.id}, ${this.name}`);
   }
 }
