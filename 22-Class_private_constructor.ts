ceb40ae1374d917ec16cd67038f947a0084edc6d
CatzillaOrz <catzillaorz@outlook.com>
2022-07-20 11:22:42 +0800

    1 file added

---
+++ 22-Class_private_constructor.ts
@@ -0,0 +1,30 @@
+class ClassPrivateConstructor {
+  constructor() {}
+}
+
+class ExtendClassPrivateContructorSingleton extends ClassPrivateConstructor {
+  /* [private static property] */
+  private static instance: ExtendClassPrivateContructorSingleton;
+  // [private constructor]
+  private constructor(public name: string) {
+    super();
+  }
+
+  // [static function]
+  static getInstance() {
+    if (ExtendClassPrivateContructorSingleton.instance) {
+      return this.instance;
+    }
+    this.instance = new ExtendClassPrivateContructorSingleton('Alice');
+    return this.instance;
+  }
+}
+
+/*
+ **    private constructor can't be initialized by [new]
+ */
+
+// let privateClass =  new ExtendClassPrivateContructorSingleton('Alice'); // Error
+
+const singletonClass: ExtendClassPrivateContructorSingleton =
+  ExtendClassPrivateContructorSingleton.getInstance();
