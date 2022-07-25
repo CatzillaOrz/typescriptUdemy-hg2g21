945203a410dae11fea1aeffa876238f3e9c05d00
CatzillaOrz <catzillaorz@outlook.com>
2022-07-25 10:26:03 +0800

    1 file added

---
+++ 30-Type_Casting.ts
@@ -0,0 +1,20 @@
+/*
+ **
+ **  [Type casting]
+ **
+ **
+ **
+ */
+
+/* Mighht conflit with react.js */
+const userInputElement = <HTMLInputElement>(
+  document.getElementById('user-input')
+);
+
+userInputElement.value = 'Hi there!';
+
+/* version 2 */
+
+const userInput = document.getElementById('user-input') as HTMLInputElement;
+
+userInput.value = 'hi there!';
