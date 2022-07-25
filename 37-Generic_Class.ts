8ff351b7cd99bf5cdafa20ef044a7e3222e18ff5
CatzillaOrz <catzillaorz@outlook.com>
2022-07-25 14:28:20 +0800

    2 files added

---
+++ 37-Generic_Class.ts
@@ -0,0 +1,41 @@
+/*
+ **  [Generic Class]
+ **
+ **
+ */
+
+class GenericClassStorage<T> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems(): T[] {
+    return [...this.data];
+  }
+}
+
+/*
+ **  user age version 1
+ **
+ **
+ */
+
+const useAsString = new GenericClassStorage<string>();
+
+useAsString.addItem('Hello');
+useAsString.addItem('World!');
+
+useAsString.getItems();
+
+const useAsUnmber = new GenericClassStorage<number>();
+
+useAsUnmber.addItem(1);
+useAsUnmber.addItem(109);
+
+useAsUnmber.getItems();
