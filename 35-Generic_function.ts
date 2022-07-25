e52f2b3ea43c45a9e479583c074ace68503ccf67
CatzillaOrz <catzillaorz@outlook.com>
2022-07-25 12:37:09 +0800

    2 files modified

--- 35-Generic_function.ts
+++ 35-Generic_function.ts
@@ -18,3 +18,24 @@ function merge<T extends object, U extends object | string | number>(
 const mergeObj = merge({ name: 'Max', hobbies: ['sports'] }, { age: 30 });
 
 console.log(mergeObj);
+
+/*
+ **  [Generic Funtion type usage]:
+ **  [√] <T extends InterfaceX>
+ **  [√] return Type : [T, string]
+ **
+ */
+
+interface Length {
+  length: number;
+}
+export function countAndDescribe<T extends Length>(element: T): [T, string] {
+  let descriptionText = 'Got no value';
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element.';
+  } else {
+    descriptionText = 'Got' + element.length + ' elements.';
+  }
+
+  return [element, descriptionText];
+}
