81db5c3084d73bb41671c3f0d1ad18fdc0fe380d
CatzillaOrz <catzillaorz@outlook.com>
2022-07-26 14:55:23 +0800

    1 file modified

--- 49-Decorator_Validation.ts
+++ 49-Decorator_Validation.ts
@@ -4,8 +4,35 @@
  **
  */
 
+/* Interface */
+interface ValidatorConfig {
+  [property: string]: {
+    [validatableProp: string]: string[]; // ['required', 'positive']
+  };
+}
+/* Validator Config */
+const registerdValidators: ValidatorConfig = {};
+
+/* required Decorator for Property */
+function Required(target: any, propName: string) {
+  registerdValidators[target.constructor.name] = {
+    ...registerdValidators[target.constructor.name],
+    [propName]: ['required'],
+  };
+}
+
+/* positive Decorator for Property */
+function PositiveNumber(target: any, propName: string) {
+  registerdValidators[target.constructor.name] = {
+    ...registerdValidators[target.constructor.name],
+    [propName]: ['positive'],
+  };
+}
+
 class DecoratorValidationCourse {
+  @Required
   title: string;
+  @PositiveNumber
   price: number;
 
   constructor(t: string, p: number) {
@@ -14,27 +41,24 @@ class DecoratorValidationCourse {
   }
 }
 
-interface ValidatorConfig {
-  [property: string]: {
-    [validatableProp: string]: string[]; // ['required', 'positive']
-  };
-}
-
-const registerdValidators: ValidatorConfig = {};
-
-function Required(target: any, propName: string) {
-  registerdValidators[target.constructor.name] = {
-    [propName]: ['required'],
-  };
-}
-
-function PositiveNumber(target: any, propName: string) {
-  registerdValidators[target.constructor.name] = {
-    [propName]: ['positive'];
+function validate(obj: any) {
+  const objValidatorConfig = registerdValidators[obj.constructor.name];
+  if (!objValidatorConfig) return true;
+  let isValid = true;
+  for (const prop in objValidatorConfig) {
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case 'required':
+          isValid = isValid && !!obj[prop];
+          break;
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
   }
+  return isValid;
 }
-
-function validate(obj: object) {}
 
 export function run49() {
   const courseForm = document.querySelector('#formgroup');
@@ -47,6 +71,11 @@ export function run49() {
     const price = +priceEl.value;
 
     const createCouse = new DecoratorValidationCourse(title, price);
+
+    if (!validate(createCouse)) {
+      alert('Please check input!');
+      return;
+    }
 
     console.log(createCouse);
   });
