4cbc9470d2b4acfc8dd0ebb57563b6176261d001
CatzillaOrz <catzillaorz@outlook.com>
2022-08-01 17:45:05 +0800

    2 files modified

--- 50-ClassTransformer.ts
+++ 50-ClassTransformer.ts
@@ -1,13 +1,34 @@
+/*
+ **  useage of
+ **   [class-validator]
+ */
+
+import { IsNotEmpty, IsNumber, IsPositive } from 'class-validator';
 export class ClassTransformerProduct {
-  constructor(private title: string, private price: number) {}
+  @IsNotEmpty()
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  constructor(title: string, price: number) {}
 
   getInformation() {
     return [this.title, `$${this.price}`];
   }
 }
 
+/*
+ **  useage of
+ **  [reflect-metadata]
+ **  [class-transformer]
+ **   [class-validator]
+ */
 import 'reflect-metadata';
-import { plainToClass, plainToInstance } from 'class-transformer';
+import { plainToInstance } from 'class-transformer';
+
+import { validate } from 'class-validator';
 
 export function run50() {
   const products = [
@@ -17,4 +38,14 @@ export function run50() {
 
   const list = plainToInstance(ClassTransformerProduct, products);
   list.forEach((e) => console.log(e.getInformation()));
+
+  /* useage [validate] */
+
+  const newProd = new ClassTransformerProduct('', 2.22);
+  validate(newProd).then((error) => {
+    if (error.length > 0) {
+      console.log('Error Found:');
+      console.log(error);
+    }
+  });
 }
